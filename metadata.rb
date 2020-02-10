1bf7fce90124cae6d97b1dae0a417bc8505d6475
Montague, Brent <bmontague@cvent.com>
2020-02-10 14:54:43 -0500

    Update for fun

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'bmontague@cvent.com'
 license 'Apache 2.0'
 description 'Installs/Configures the workup framework'
 long_description 'Installs/Configures workup framework'
-version '0.0.1'
+version '1.0.0'
 
 issues_url 'https://github.com/cvent/workup-framework/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/cvent/workup-framework' if respond_to?(:source_url)
 
-depends 'ohai'
+depends 'ohai', '< 5.3.0'
